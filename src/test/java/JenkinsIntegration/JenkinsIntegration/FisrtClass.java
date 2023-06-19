bc28f9957c55988a1ccc3eb363e2ad5629662893
durgaprasad <katakatladurgaprasad@gmail.com>
2023-06-19 08:37:34 +0530

    First Commit

---
+++ src/test/java/JenkinsIntegration/JenkinsIntegration/FisrtClass.java
@@ -0,0 +1,14 @@
+package JenkinsIntegration.JenkinsIntegration;
+
+import org.testng.annotations.Test;
+
+public class FisrtClass {
+	@Test
+	public void FirstTest() {
+
+		System.out.println("Chinna");
+		System.out.println("Chinna123");
+
+	}
+
+}
---
+++ src/test/java/JenkinsIntegration/JenkinsIntegration/SecondClass.java
@@ -0,0 +1,15 @@
+package JenkinsIntegration.JenkinsIntegration;
+
+import org.testng.annotations.Test;
+
+public class SecondClass {
+    @Test
+	private void SecondTest() {
+		
+
+		System.out.println("durga prasad");
+		System.out.println("durga prasad katakatla");
+
+	}
+
+}
